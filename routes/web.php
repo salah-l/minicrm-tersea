287a360e2e1273c8edaea287cbc1d4d405f75284
Salah <salah.l@ablaviation.com>
2022-12-30 03:46:24 +0100

    create application base routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\UserController; 
+use \App\Http\Controllers\CompanyController; 
+use \App\Http\Controllers\EmployeeController; 
+use \App\Http\Controllers\InvitationController; 
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Admin
+Route::get('/', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'createUser']);
+
+Route::get('/companies/{id}', [CompanyController::class, 'getCompany']);
+Route::get('/companies', [CompanyController::class, 'getAllCompanies']);
+Route::post('/companies', [CompanyController::class, 'createCompany']);
+Route::put('/companies/{id}', [CompanyController::class, 'updateCompany']);
+Route::delete('/companies/{id}', [CompanyController::class, 'deleteCompany']);
+
+Route::post('/employees', [EmployeeController::class, 'inviteEmployee']);
+Route::get('/employees/{id}', [EmployeeController::class, 'getEmployee']);
+Route::get('/employees', [EmployeeController::class, 'getEmployees']);
+
+Route::get('/invitations', [InvitationController::class, 'getInvitations']);
+Route::put('/invitations/{id}', [InvitationController::class, 'updateInvitationStatus']);
+Route::post('/invitations', [InvitationController::class, 'sendInvitation']);
+
+
+//Employee
+Route::put('/employees/registration/{token}', [EmployeeController::class, 'setEmployeePassword']);
+Route::put('/employees/registration/details/{token}', [EmployeeController::class, 'setEmployeeDetails']);
+
+Route::get('/employees/{id}', [EmployeeController::class, 'viewEmployee']);
+Route::put('/employees/{id}', [EmployeeController::class, 'updateEmployee']);
+
+Route::get('/company/{id}', [CompanyController::class, 'viewCompany']);
+Route::get('/company/{id}/employees', [CompanyController::class, 'viewCompany']);
