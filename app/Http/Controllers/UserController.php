137fc63314c128d356db1d39d6d12450aa15ceb3
Salah <salah.l@ablaviation.com>
2022-12-30 03:45:21 +0100

    create controllers

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    
+
+    function create(Request $request){
+
+        
+
+    }
+
+
+}
