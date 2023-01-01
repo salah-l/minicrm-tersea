711a78471911c1e7f8fbfd24a4d4494da8768f55
Salah <salah.l@ablaviation.com>
2023-01-01 01:29:10 +0100

    create invitation mail template view

---
+++ resources/views/invitation.blade.php
@@ -0,0 +1,9 @@
+Exemple d'email
+
+Bonjour {{$name}},
+
+Vous etes invite a rejoindre la societe "{{$company}}" sur la platform minicrm.
+Visitez le lien ci-dessous pour vous enregistrer.
+lien: {{$registration_link}}
+
+Merci.
