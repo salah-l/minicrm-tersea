c45cc1d8b1a8dcc55e1c5683ff7ab72c3165183d
Salah <salah.l@ablaviation.com>
2023-01-03 03:28:25 +0100

    add the middlewares to Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'user.auth' => \App\Http\Middleware\UserAuthMiddleware::class,
+        'employee.auth' => \App\Http\Middleware\EmployeeAuthMiddleware::class
     ];
 }
